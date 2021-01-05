cc94b0f44e9990217db6a16b632fdf7080fab593
Sonya <sofa.chernikova@mail.ru>
2021-01-05 18:15:40 +0300

    Task №2

--- ind.js
+++ ind.js
@@ -1,3 +1,4 @@
+// Задание №1
 console.log ('Задание 1')
 let season = {
     country: 'Москва',
@@ -7,3 +8,18 @@ let season = {
 };
 console.log ('Список свойств: ' + Object.keys(season))
 console.log ('В городе ' + season.country + ' сейчас ' + season.seasonName + ', температура колеблется от ' + season.minTemp + ' до ' + season.maxTemp)
+
+// Задание №2
+console.log ('Задание 2')
+
+arr([1, 2, 3, 4, 5]);
+function arr(myArray) {
+    let len = myArray.length;
+    for (let i = 0; i < len; i++){
+        if (myArray.length > 1) {
+            console.log(myArray.pop() + ', осталось ' + myArray)
+        } else {
+            console.log(myArray[0])
+        }
+    }
+}
